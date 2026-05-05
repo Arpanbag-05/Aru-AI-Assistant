cd803027bc5a0235d601c3bd784c0ea4fe648b54
Arpan Bag <bagarpan05@gmail.com>
2026-05-05 20:03:27 +0530

    9th Tutorial Complete

--- engine/features.py
+++ engine/features.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
@@ -6,6 +7,7 @@ import eel
 
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
+import pywhatkit as kit
 
 #sound function for playing sound
 def playAssistantSound():
@@ -29,3 +31,16 @@ def openCommand(query):
 
     else:
         speak(f"{query} not found")
+
+def PlayYoutube(query):
+    search_term = extract_yt_term(query)
+    if search_term:
+        speak("Playing " + search_term + " on YouTube")
+        kit.playonyt(search_term)
+    else:
+        speak("Sorry, I couldn't find the YouTube video you requested.")
+
+def extract_yt_term(command):
+    pattern = r'play\s+(.*?)\s+on\s+youtube'
+    match = re.search(pattern, command, re.IGNORECASE)
+    return match.group(1) if match else None
