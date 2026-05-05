2858e7c2a102e4d0e8421a75b8abd44cfa4e5c18
Arpan Bag <bagarpan05@gmail.com>
2026-05-05 12:15:52 +0530

    6t Tutorial Complete

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr 
 def speak(text):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
@@ -10,4 +10,25 @@ def speak(text):
     engine.runAndWait()
 
 
-speak('Hello, I am your Aru. I am an AI assistant.')
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, timeout=10, phrase_time_limit=6)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en')
+        print(f'User said: {query}')
+
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text= takeCommand()
+    
+
+speak(text)
